b2553f7514072d37a4064209df175c1c67ea40e4
Cheese <adhesivecheese@gmail.com>
2019-08-12 21:54:02 -0400

    Okay maybe the full URL does work
    Maybe reddit was just having an issue

--- js/content-inbox/dbhelper.js
+++ js/content-inbox/dbhelper.js
@@ -55,8 +55,8 @@
 		this.request();
 	}
 	PMIndexer.prototype.request = function () {
-		//var url = 'https://old.reddit.com/message/messages.json?raw_json=1&limit=100';
-		var url = '/message/messages.json?raw_json=1&limit=100';
+		var url = 'https://old.reddit.com/message/messages.json?raw_json=1&limit=100';
+		//var url = '/message/messages.json?raw_json=1&limit=100';
 		if (typeof this.direction === "string") {
 			url += '&' + this.direction + '=' + this.reference;
 		}
